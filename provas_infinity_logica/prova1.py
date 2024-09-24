cda2deeaf73b160724e026e50381ac0b821c7e8d
Lucasvalves <lucasvalves98@gmail.com>
2024-09-24 10:59:05 -0300

    feat: first commit

---
+++ provas_infinity_logica/prova1.py
@@ -0,0 +1,13 @@
+
+#Crie um exemplo de cada uma das principais tipagens de variáveis.
+age = 26 # Inteiro
+name = "Lucas" # String
+weight = 1.76 # Float
+single = True #Boolean 
+
+
+# Crie uma solicitação de dados ao usuário
+name = input("Digite seu nome: ")
+
+#Imprima a solicitação anterior na tela com uma mensagem personalizada
+print(f"Olá {name}, seja muito bem-vindo")
