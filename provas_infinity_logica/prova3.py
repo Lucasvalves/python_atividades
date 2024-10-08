a0581584122d5f4e2d69791eceed930a9914481b
Lucasvalves <lucasvalves98@gmail.com>
2024-10-08 09:38:22 -0300

    feat: adding file prova4.py

--- provas_infinity_logica/prova3.py
+++ provas_infinity_logica/prova3.py
@@ -1,11 +1,17 @@
-number = int(input("informe um número inteiro de 1 a 10: ")) 
 
-if number > 10 or number <= 0:
-  print(f"Número invalido. por favor informe um número de 1 a 10")
+numbers = -1
+sun = 0
+count = 0
 
-else:
-  print(f"Tabuada de {number}: ")
-  for i in range(1,11):
-    print(f"{number} X {i} = {number * i}")
+while numbers != 0:
+  numbers = int(input("Informe o número: "))
+  
+  if numbers != 0:
+    sun = sun + numbers
+    count += 1
+  
+print(f"Quantidade de números digitados: {count}")
+print(f"Soma total: {sun}")
+print(f"Média artimética: {sun/count}")
 
 
